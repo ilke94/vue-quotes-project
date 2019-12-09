49f8a2516e5b9e14d2a1017afe7c753e94d5941f
ilke94 <milos1802@gmail.com>
2019-12-09 23:45:42 +0100

    second commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+    export default {
+        components:{
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        },
+        data() {
+            return {
+                quotes: [
+                    'Test quote'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote){
+                this.quotes.push(quote);
+            }
+        },
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <app-quote v-for="quote in quotes" v-bind:key="quote">{{quote}}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+export default {
+  components:{
+    appQuote: Quote
+  },
+  props: ['quotes']
+}
+</script>
+
+<style>
+
+</style>
